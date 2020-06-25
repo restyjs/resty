f29d0e7852d25c1bfe2b978d79b5c70b14cfe57c
Satish B <satish.babariya@gmail.com>
2020-06-25 18:28:37 +0530

    refactor packages

---
+++ example/main.ts
@@ -0,0 +1,21 @@
+import resty, { Controller, Get, Context } from "../src";
+
+@Controller("/hello")
+class HelloController {
+  @Get("/")
+  index() {
+    return "Hello World";
+  }
+
+  @Get("/health")
+  health(ctx: Context) {
+    return ctx.res.json({ status: "ok" }).status(200);
+  }
+}
+
+const app = resty({
+  routePrefix: "/api",
+  controllers: [HelloController],
+});
+
+app.listen(8080);
