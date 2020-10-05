e5fb399b45b01790f6a1f5ad192bcde53873f8e5
Satish B <satish.babariya@gmail.com>
2020-10-05 18:04:53 +0530

    updated default errors format

--- src/handlers.ts
+++ src/handlers.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { HTTPError, ValidationError } from "./errors";
+import { Exception, HTTPError, ValidationError } from "./errors";
 
 export const NotFoundErrorHandler: express.RequestHandler = (
   req: express.Request,
@@ -16,25 +16,43 @@ export const DefaultErrorHandler: express.ErrorRequestHandler = (
   res: express.Response,
   next: express.NextFunction
 ) => {
-  if (err instanceof ValidationError) {
-    res.status(400);
+  if (err instanceof Exception) {
+    res.status(err.status || 500);
     res.json({
-      error: err,
+      errors: {
+        message: err.message,
+      },
     });
-    return;
-  } else if (err instanceof HTTPError) {
-    res.status(err.statusCode);
-    res.json({
-      error: err,
-    });
-    return;
+  } else {
+    next(err);
   }
-
-  res.status(500);
-  res.json({
-    error: {
-      statusCode: 500,
-      message: err.message ? err.message : err,
-    },
-  });
 };
+
+// export const DefaultErrorHandler: express.ErrorRequestHandler = (
+//   err: Error,
+//   req: express.Request,
+//   res: express.Response,
+//   next: express.NextFunction
+// ) => {
+//   if (err instanceof ValidationError) {
+//     res.status(400);
+//     res.json({
+//       error: err,
+//     });
+//     return;
+//   } else if (err instanceof HTTPError) {
+//     res.status(err.status);
+//     res.json({
+//       error: err,
+//     });
+//     return;
+//   }
+
+//   res.status(500);
+//   res.json({
+//     error: {
+//       statusCode: 500,
+//       message: err.message ? err.message : err,
+//     },
+//   });
+// };
